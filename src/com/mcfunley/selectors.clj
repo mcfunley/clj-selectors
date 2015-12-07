650e09985a03a22bdae086edb8bdf50dc4162027
Dan McKinley <mcfunley@gmail.com>
2015-12-06 19:04:22 -0800

    parsing attributes and pseudo-elements working

--- src/com/mcfunley/selectors.clj
+++ src/com/mcfunley/selectors.clj
@@ -2,7 +2,89 @@
   (:require [clojure.core.match :refer [match]]
             [clojure.core.match.regex :refer :all]))
 
+
+(import java.lang.UnsupportedOperationException)
+
+
+(defn unsupported
+  [msg]
+  (throw (UnsupportedOperationException. (str msg))))
+
+
+
+;; =============================================================================
+;; tokenizing
+
+
+(defn group-extract
+  [pattern s]
+  (filter some? (flatten (map rest (re-seq pattern s)))))
+
+
+(defn extract-quoted
+  ;; pulls quoted strings out of the input, e.g.
+  ;; (extract-quoted "foo  bar \"baz\"") -> ("foo  bar " "\"baz\"")
+  [s]
+  (group-extract #"([^\"]+)?(\"[^\"]+\")?" s))
+
+
+(defn extract-bracketed
+  [s]
+  (group-extract #"([^\[]+)?(\[[^\[]+\])?" s))
+
+
+
+(defn tokenize-selector
+  "The first pass tokenizer run on a whole selector expression."
+  [selector]
+
+  (defn replace-in-brackets
+    [g pattern repl]
+    (match g
+      #"^\[.*\]$" (clojure.string/replace g pattern repl)
+      :else g))
+
+  (defn hack-brackets [g] (replace-in-brackets g #"\s" "\0"))
+  (defn unhack-brackets [g] (clojure.string/replace g #"\x00" " "))
   
+  (let [bracket-groups (extract-bracketed selector)
+        hacked-brackets (map hack-brackets bracket-groups)
+        joined (clojure.string/join hacked-brackets)
+        element-tokens (clojure.string/split joined #"\s+")]
+    (map unhack-brackets (filter not-empty element-tokens))))
+
+
+
+(defn tokenize-element
+  "Tokenizes an element expression (e.g. `foo.bar#baz`)"
+  [el]
+  (when el
+    (match (re-matches #"(.*)(\[[^\]]+\])(.*)?" el)
+      [_ start attr-exp rest] (concat
+                                 (tokenize-element start)
+                                 (list attr-exp)
+                                 (tokenize-element rest))
+    
+      :else (map first (re-seq #"(::|[.:#])?[^.:#]+" el)))))
+
+
+(defn tokenize-attribute-selector
+  [sel]
+
+  (defn split-words
+    [s]
+    (match s
+      #"^\"[^\"]+\"$" (clojure.string/replace s #"\"" "")
+      :else           (clojure.string/split (clojure.string/trim s) #"\s+")))
+  
+  (let [expr (clojure.string/replace sel #"[\[\]]" "")
+        trimmed (clojure.string/trim expr)
+        padded (clojure.string/replace trimmed #"([~|$\^*]?=)" " $1 ")
+        quoted-separated (extract-quoted padded)]
+    (flatten (map split-words quoted-separated))))
+
+
+
 
 ;; =============================================================================
 ;; parsing
@@ -10,87 +92,123 @@
 (declare consume-tokens)
 
 
-(defn tokenize-element
-  [token]
-  (when token
-    (match (re-matches #"(.*)(\[[^\]]+\])(.*)?" token)
-      [_ start attr-exp rest] (concat
-                                 (tokenize-element start)
-                                 (list attr-exp)
-                                 (tokenize-element rest))
-    
-      :else (re-seq #"[.:#]?[^.:#]+" token))))
-
-
-(defn parse-attribute-selector
-  [token]
-  :todo-attribute-selector)
-
-(defn parse-pseudo-element
-  [token]
-  :todo-pseudo-element)
-
-(defn parse-pseudo-class
-  [token]
-  :todo-pseudo-class)
-
-(defn parse-token
-  [token]
-  (match token
-    "*"           `(match-element-type :any)
-    #"^\[.*\]$"   (parse-attribute-selector token)
-    #"\.[^:#]+$"  `(match-class ~(subs token 1))
-    #"#[^.:]+$"   `(match-id ~(subs token 1))
-    #"::.*"       (parse-pseudo-element token)
-    #":[^:].*"    (parse-pseudo-class token)
-    #"^[^\[:.#]+" `(match-element-type ~token)
-    :else         `(match-all ~(map parse-token (tokenize-element token)))))
-
-
 (defn symbol-to-matcher
   [sym]
   (symbol "com.mcfunley.selectors" (name sym)))
 
 
-(defn build-binary-op
-  [matcher-sym tokens]
-  (let [matcher (symbol-to-matcher matcher-sym)
-        left-side (parse-token (first tokens))
-        right-side (consume-tokens (drop 2 tokens))]
-    `(~matcher ~left-side ~right-side)))
+(defn read-attribute-selector-tokens
+  [tokens]
+
+  (defn collect-expressions
+    [expression rest-tokens]
+    (let [rest-expressions (read-attribute-selector-tokens rest-tokens)]
+      (cons expression (filter some? (list rest-expressions)))))
+
+  (defn binop
+    [matcher-sym a b r]
+    (let [matcher (symbol-to-matcher matcher-sym)
+          op `(~matcher ~a ~b)]
+      (if (empty? r)
+        op
+        `(match-all ~(collect-expressions op r)))))
+       
+  (match (vec tokens)
+    []             nil
+    
+    [a "~=" b & r] (binop 'match-attribute-value-contains-word a b r)
+    [a "^=" b & r] (binop 'match-attribute-value-begins a b r)
+    [a "$=" b & r] (binop 'match-attribute-value-ends a b r)
+    [a "*=" b & r] (binop 'match-attribute-value-contains-pattern a b r)
+    [a "|=" b & r] (binop 'match-attribute-value-lang-subcode a b r)
+    [a "=" b & r]  (binop 'match-attribute-value a b r)
+    
+    [a & r]        `(match-all
+                     ~(collect-expressions `(match-attribute-exists ~a) r))))
+                     
+
+
+(defn parse-attribute-selector
+  [sel]
+  (read-attribute-selector-tokens (vec (tokenize-attribute-selector sel))))
+
+
+(defn parse-pseudo-element
+  [token]
+  (match token
+    "::first-line"    `(select-first-line)
+    "::first-letter"  `(select-first-letter)
+    "::before"        (unsupported "::before pseudo element not supported.")
+    "::after"         (unsupported "::after pseudo element not supported.")))
+
+
+(defn parse-pseudo-class
+  [token]
+  :todo-pseudo-class)    
+
+
+(defn parse-token
+  [token]
+  (match token
+    "*"               `(match-element-type :any)
+    #"^\[.*\]$"       (parse-attribute-selector token)
+    #"^\.[^\[:#]+$"   `(match-class ~(subs token 1))
+    #"#[^\[.:]+$"       `(match-id ~(subs token 1))
+    #"^::.*"          (parse-pseudo-element token)
+    #"^:[^:].*"       (parse-pseudo-class token)
+    #"^[^\[:.#]+"     `(match-element-type ~token)
+    :else             `(match-all ~(map parse-token (tokenize-element token)))))
 
 
 (defn consume-tokens
   [tokens]
 
+  (defn binop
+    [matcher-sym tokens]
+    (let [matcher (symbol-to-matcher matcher-sym)
+          left-side (parse-token (first tokens))
+          right-side (consume-tokens (drop 2 tokens))]
+    `(~matcher ~left-side ~right-side)))
+
   (match (vec tokens)
     []          ()
     [a]         (parse-token a)
-    [_ ">" _]   (build-binary-op 'match-with-child tokens)
-    [_ "+" _]   (build-binary-op 'match-immediately-preceding tokens)
-    [_ "~" _]   (build-binary-op 'match-preceding tokens)
+    [_ ">" _]   (binop 'match-with-child tokens)
+    [_ "+" _]   (binop 'match-immediately-preceding tokens)
+    [_ "~" _]   (binop 'match-preceding tokens)
     [a & r]     `(match-ancestor ~(parse-token a) ~(consume-tokens r))))
-
-
-(defn tokenize-selector
-  [selector]
-  (filter not-empty (clojure.string/split selector #"\s+")))
-
-
-(defn parse-selector
-  [selector]
-  (let [tokens (tokenize-selector selector)]
-    (consume-tokens tokens)))
 
 
 
 ;; =============================================================================
-;; compiling
+;; simplifying
 
-(defn compile-selector
-  [selector]
-  (let [ast (parse-selector selector)]
-    ))
+(defn match-all? [x] (= (first x) `match-all))
+
+
+(defn flatten-match-all
+  [match-all-expression]
+  (cons `match-all
+        (list 
+         (filter (complement match-all?)
+                 (tree-seq match-all? second match-all-expression)))))
+   
+(defn simplify
+  [expr-tree]
+
+  (defn simplify-expressions
+    [argument-list]
+    (map (fn [a] (if (sequential? a) (simplify a) a)) argument-list))
+
+  (match (vec expr-tree)
+    ;; if we're at a match-all, flatten any children that are match-all into
+    ;; the list and recurse down.
+    [`match-all _]   (flatten-match-all expr-tree)
+
+    ;; recurse down if its an expression
+    [f & arguments]  `(~f ~@(simplify-expressions arguments))
+
+    :else            expr-tree))
 
 
 ;; =============================================================================
@@ -101,6 +219,17 @@
   (let [[tag attributes children] elem]
     (filter some? (cons (matcher root parent elem)
                         (map #(match-element root elem %1 matcher) children)))))
+
+
+;; =============================================================================
+;; public interface
+
+
+(defn compile-selector
+  [selector]
+  (simplify
+   (let [tokens (tokenize-selector selector)]
+     (consume-tokens tokens))))
 
 
 (defn $
--- test/com/mcfunley/selectors_test.clj
+++ test/com/mcfunley/selectors_test.clj
@@ -4,72 +4,206 @@
 
 (alias 'selectors 'com.mcfunley.selectors)
 
-(deftest parse-any
+;; ==============================================================================
+;; compilation tests
+
+(deftest compile-any
   (is (= `(selectors/match-element-type :any)
-         (parse-selector "*"))))
+         (compile-selector "*"))))
 
-(deftest parse-element-type
+(deftest compile-element-type
   (is (= `(selectors/match-element-type "foo")
-         (parse-selector "foo"))))
+         (compile-selector "foo"))))
 
-(deftest parse-with-child
+(deftest compile-with-child
   (is (= `(selectors/match-with-child
            (selectors/match-element-type "foo")
            (selectors/match-element-type "bar"))
-         (parse-selector "foo > bar"))))
+         (compile-selector "foo > bar"))))
 
-(deftest parse-immediately-preceding
+(deftest compile-immediately-preceding
   (is (= `(selectors/match-immediately-preceding
            (selectors/match-element-type "foo")
            (selectors/match-element-type "bar"))
-         (parse-selector "foo + bar"))))
+         (compile-selector "foo + bar"))))
 
-(deftest parse-preceding
+(deftest compile-preceding
   (is (= `(selectors/match-preceding
            (selectors/match-element-type "foo")
            (selectors/match-element-type "bar"))
-         (parse-selector "foo ~ bar"))))
+         (compile-selector "foo ~ bar"))))
 
-(deftest parse-extra-whitespace
+(deftest compile-extra-whitespace
   (is (= `(selectors/match-preceding
            (selectors/match-element-type "foo")
            (selectors/match-element-type "bar"))
-         (parse-selector "foo   ~ bar "))))
+         (compile-selector "foo   ~ bar "))))
 
-(deftest parse-ancestor
+(deftest compile-ancestor
   (is (= `(selectors/match-ancestor
            (selectors/match-element-type "foo")
            (selectors/match-element-type "bar"))
-         (parse-selector "foo bar"))))
+         (compile-selector "foo bar"))))
 
-(deftest parse-ancestor-class-match
+(deftest compile-ancestor-class-match
   (is (= `(selectors/match-ancestor
            (selectors/match-element-type "foo")
            (selectors/match-class "bar"))
-         (parse-selector "foo .bar"))))
+         (compile-selector "foo .bar"))))
 
-(deftest parse-type-and-id
+(deftest compile-type-and-id
   (is (= `(selectors/match-all
            ((selectors/match-element-type "foo")
             (selectors/match-id "bar")))
-         (parse-selector "foo#bar"))))
+         (compile-selector "foo#bar"))))
 
-(deftest parse-type-id-and-class
+(deftest compile-type-id-and-class
   (is (= `(selectors/match-all
            ((selectors/match-element-type "foo")
             (selectors/match-id "bar")
             (selectors/match-class "baz")))
-         (parse-selector "foo#bar.baz"))))
+         (compile-selector "foo#bar.baz"))))
 
-;; (deftest parse-having-attribute
-;;   (is (= `(every? (selectors/match-element-type "foo")
-;;                   (selectors/match-attribute "bar"))
-;;          (parse-selector "foo[bar]"))))
+(deftest compile-any-with-class
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type :any)
+            (selectors/match-class "foo")))
+         (compile-selector "*.foo"))))
+
+(deftest compile-first-line
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type "foo")
+            (selectors/select-first-line))))
+      (compile-selector "foo::first-line")))
+
+(deftest compile-first-letter
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type "foo")
+            (selectors/match-class "bar")
+            (selectors/select-first-letter))))
+      (compile-selector "foo.bar::first-letter")))
 
 
-(deftest tokenize-filter-exp
+(deftest compile-having-attribute
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type "foo")
+            (selectors/match-attribute-exists "bar")))
+         (compile-selector "foo[bar]"))))
+
+(deftest compile-having-two-attributes
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type "foo")
+            (selectors/match-attribute-exists "bar")
+            (selectors/match-attribute-exists "baz")))
+         (compile-selector "foo[bar baz]"))))
+
+(deftest compile-attribute-contains-word
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type "foo")
+            (selectors/match-attribute-value-contains-word "bar" "baz")))
+         (compile-selector "foo[bar ~= \"baz\"]"))))
+
+(deftest compile-attribute-value-begins
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type "foo")
+            (selectors/match-class "bar")
+            (selectors/match-attribute-value-begins "baz" "fizz buzz")))
+         (compile-selector "foo.bar[baz^=\"fizz buzz\"]"))))
+
+(deftest compile-attribute-value-ends
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type :any)
+            (selectors/match-attribute-value-ends "foo" "bar")))
+         (compile-selector "*[foo $= \"bar\"]"))))
+
+(deftest compile-attribute-value-contains-pattern
+  (is (= `(selectors/match-all
+           ((selectors/match-element-type :any)
+            (selectors/match-id "crap")
+            (selectors/match-attribute-value-contains-pattern "foo" "bar")))
+         (compile-selector "*#crap[foo *=   \"bar\"]"))))
+
+(deftest compile-wacky-attribute-value
+  (is (= `(selectors/match-all
+           ((selectors/match-class "foo")
+            (selectors/match-attribute-exists "bar")
+            (selectors/match-attribute-value-lang-subcode "baz" "en")
+            (selectors/match-attribute-exists "goo")))
+         (compile-selector ".foo[bar baz|=\"en\" goo  ]"))))
+
+(deftest compile-match-attribute-value
+  (is (= `(selectors/match-all
+           ((selectors/match-id "foo")
+            (selectors/match-attribute-exists "bar")
+            (selectors/match-attribute-value "baz" "goo ball")
+            (selectors/match-attribute-value-contains-pattern "qux" "quick")))
+         (compile-selector "#foo[bar baz=\"goo ball\" qux *= \"quick\"]"))))
+
+(deftest compile-complicated-first-line
+  (is (= `(selectors/match-ancestor
+           (selectors/match-all
+            ((selectors/match-id "foo")
+             (selectors/match-attribute-value "bar" "baz")))
+           (selectors/match-all
+            ((selectors/match-class "qux")
+             (selectors/select-first-line))))
+         (compile-selector "#foo[bar=\"baz\"] .qux::first-line"))))
+
+(deftest compile-first-line
+  (is (= `(selectors/match-all
+           ((selectors/match-class "foo")
+            (selectors/select-first-line)))
+         (compile-selector ".foo::first-line"))))
+
+(deftest compile-first-letter
+  (is (= `(selectors/match-with-child
+           (selectors/match-element-type "foo")
+           (selectors/match-all
+            ((selectors/match-id "bar")
+             (selectors/select-first-letter))))
+         (compile-selector "foo > #bar::first-letter"))))
+
+;; ==============================================================================
+;; tokenizing tests
+
+; tokenize-selector
+
+(deftest tokenize-selector-simple
+  (is (= (tokenize-selector "foo") '("foo"))))
+
+(deftest tokenize-selector-classname
+  (is (= (tokenize-selector ".foo") '(".foo"))))
+
+(deftest tokenize-selector-whitespace
+  (is (= (tokenize-selector "  foo .bar") '("foo" ".bar"))))
+
+(deftest tokenize-selector-with-attribute-selector
+  (is (= (tokenize-selector "foo[bar] #baz") '("foo[bar]" "#baz"))))
+
+(deftest tokenize-selector-attribute-selector-whitespace
+  (is (= (tokenize-selector "foo[bar baz].goo") '("foo[bar baz].goo"))))
+
+(deftest tokenize-selector-attribute-selector-whitespace-multiple
+  (is (= (tokenize-selector "foo[bar baz] .goo") '("foo[bar baz]" ".goo"))))
+
+(deftest tokenize-selector-wacky-attribute-selector
+  (is (= (tokenize-selector ".foo[bar baz|=\"en\" goo  ]")
+         '(".foo[bar baz|=\"en\" goo  ]"))))
+
+
+; tokenize-element
+
+(deftest tokenize-element-attribute-selector
   (is (= (tokenize-element "foo[bar]")
          '("foo" "[bar]"))))
+
+(deftest tokenize-attribute-selector-with-whitespace
+  (is (= (tokenize-element "foo[bar baz].goo")
+         '("foo" "[bar baz]" ".goo"))))
+
+(deftest tokenize-attribute-selector-with-whitespace-2
+  (is (= (tokenize-element "foo[ bar baz *=goo  fizz].goo")
+         '("foo" "[ bar baz *=goo  fizz]" ".goo"))))
 
 (deftest tokenize-id-and-class
   (is (= (tokenize-element "foo.bar#baz")
@@ -82,3 +216,141 @@
 (deftest tokenize-class-id-filter-exp
   (is (= (tokenize-element "foo[bar].baz#goo")
          '("foo" "[bar]" ".baz" "#goo"))))
+
+(deftest tokenize-pseudo-element
+  (is (= (tokenize-element "foo::bar")
+         '("foo" "::bar"))))
+
+(deftest tokenize-element-wacky-attribute-selector
+  (is (= (tokenize-element ".foo[bar baz|=\"en\" goo  ]")
+         '(".foo" "[bar baz|=\"en\" goo  ]"))))
+
+
+; tokenize-attribute-selector
+
+(deftest tokenize-attribute-selector-1
+  (is (= (tokenize-attribute-selector "[foo bar~=\"baz\"]")
+         '("foo" "bar" "~=" "baz"))))
+
+(deftest tokenize-attribute-selector-attrname
+  (is (= (tokenize-attribute-selector "[foo]")
+         '("foo"))))
+
+(deftest tokenize-attribute-selector-internal-spaces
+  (is (= (tokenize-attribute-selector "[foo bar ^= \"baz\"   goo]")
+         '("foo" "bar" "^=" "baz" "goo"))))
+
+(deftest tokenize-attribute-selector-trimmed
+  (is (= (tokenize-attribute-selector "[  foo bar ]")
+         '("foo" "bar"))))
+
+(deftest tokenize-attribute-selector-quoted-string
+  (is (= (tokenize-attribute-selector "[foo  bar=\"baz  goo\"]")
+         '("foo" "bar" "=" "baz  goo"))))
+
+(deftest tokenize-attribute-selector-ending-spaces-ends-with-quote
+  (is (= (tokenize-attribute-selector "[foo*=\"baz\" goo bar=\" adf \"  ]")
+         '("foo" "*=" "baz" "goo" "bar" "=" " adf "))))
+          
+(deftest tokenize-attribute-selector-language-subcode
+  (is (= (tokenize-attribute-selector "[foo|=\"bar\"]")
+         '("foo" "|=" "bar"))))
+
+(deftest tokenize-wacky-attribute-selector
+  (is (= (tokenize-attribute-selector "[bar baz|=\"en\" goo  ]")
+         '("bar" "baz" "|=" "en" "goo"))))
+
+
+;; ==============================================================================
+;; random regressions
+
+(deftest parse-attribute-selector-match-all-nesting
+  (is (= `(selectors/match-all
+           ((selectors/match-attribute-exists "foo")
+            (selectors/match-all
+             ((selectors/match-attribute-exists "bar")))))
+         (parse-attribute-selector "[foo bar]"))))
+
+(deftest parse-wacky-attribute-selector
+  (is (= `(selectors/match-all
+           ((selectors/match-attribute-exists "bar")
+            (selectors/match-all
+             ((selectors/match-attribute-value-lang-subcode "baz" "en")
+              (selectors/match-all
+               ((selectors/match-attribute-exists "goo"))))))
+         (parse-attribute-selector "[bar baz|=\"en\" goo  ]")))))
+
+
+;; ==============================================================================
+;; simplification
+
+(deftest flatten-match-all-nop
+  (is (= '(com.mcfunley.selectors/match-all ((foo bar)))
+         (flatten-match-all
+          '(com.mcfunley.selectors/match-all ((foo bar)))
+          ))))
+
+(deftest flatten-match-all-one-level
+  (is (= '(com.mcfunley.selectors/match-all
+           ((foo bar)
+            (baz goo)
+            (fizz buzz)
+            (qux quick)))
+
+         (flatten-match-all
+          '(com.mcfunley.selectors/match-all
+            ((foo bar)
+             (com.mcfunley.selectors/match-all
+              ((baz goo)
+               (fizz buzz)))
+             (qux quick)))))))
+
+(deftest flatten-match-all-two-levels
+  (is (= '(com.mcfunley.selectors/match-all
+           ((foo bar)
+            (baz goo)
+            (fizz buzz)
+            (qux quick)
+            (quack quack)))
+
+         (flatten-match-all
+          '(com.mcfunley.selectors/match-all
+            ((foo bar)
+             
+             (com.mcfunley.selectors/match-all
+              ((com.mcfunley.selectors/match-all ((baz goo)))
+               (com.mcfunley.selectors/match-all ((fizz buzz)))
+               (qux quick)))
+
+             (com.mcfunley.selectors/match-all ((quack quack)))))))))
+
+
+(deftest simplify-simple
+  (is (= '(foo (bar baz))
+         (simplify '(foo (bar baz))))))
+
+(deftest simplify-match-all
+  (is (= '(com.mcfunley.selectors/match-all
+           ((foo bar)
+            (baz goo)))
+
+         (simplify
+          '(com.mcfunley.selectors/match-all
+            ((foo bar)
+             (com.mcfunley.selectors/match-all ((baz goo)))))))))
+
+(deftest simplify-buried-match-alls
+  (is (= '(foo
+           (bar (com.mcfunley.selectors/match-all ((baz goo) (fizz buzz))))
+           (bar (quack quack)))
+
+         (simplify
+          '(foo
+            (bar (com.mcfunley.selectors/match-all
+                  ((com.mcfunley.selectors/match-all ((baz goo)))
+                   (fizz buzz))))
+            (bar (quack quack)))))))
+
+(deftest simplify-single-select
+  (is (= '(com.mcfunley.selectors/select-first-line)
+         (simplify '(com.mcfunley.selectors/select-first-line)))))
